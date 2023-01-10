c0db87bf825a3c555f722d6461941c1884900d34
grantgariepy <grantgariepy@protonmail.com>
2023-01-09 19:22:55 -0700

    dynamic filter for ingr (case sensitive for now)

--- src/routes/ingredients/+page.svelte
+++ src/routes/ingredients/+page.svelte
@@ -1,37 +1,46 @@
 <script lang="ts">
-  export let data:any;
+	import { writable, derived } from 'svelte/store';
+	export let data: any;
+
+	const array = data.drinks;
+	const drinkArray: any = [];
+	array.forEach((element: any) => {
+		drinkArray.push(element.strIngredient1.toLowerCase());
+	});
+	console.log(drinkArray);
+	const term = writable('');
+	const items = writable(drinkArray);
+	const filtered = derived([term, items], ([$term, items]) =>
+		$items.filter((x: any) => x.includes($term))
+	);
+	let val = '';
+	$: term.set(val);
 </script>
 
 <div>
-  <div class='bg-white py-6 sm:py-8 lg:py-12'>
-    <div class='max-w-screen-2xl px-4 md:px-8 mx-auto'>
-      <div class='flex justify-between items-end gap-4 mb-6'>
-        <h2 class='text-gray-800 text-2xl lg:text-3xl font-bold'>Ingredients</h2>
-      </div>
-      <div class='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-8'>
-        <!-- product - start  -->
-        {#each data.drinks as drink}
-        <div>
-          <a
-            href={`ingredients/${drink.strIngredient1}`}
-            class='group h-80 block bg-gray-100 rounded-lg overflow-hidden relative mb-2 lg:mb-3'
-          >
-            <!-- <img
-              src={drink.strCategoryThumb}
-              alt='drinks'
-              class='w-full h-full object-cover object-center group-hover:scale-110 transition duration-200'
-              loading='lazy'
-            /> -->
-          </a>
-          <div>
-            <div class='flex items-end gap-2'>
-              <span class='text-gray-800 lg:text-lg font-bold'>{drink.strIngredient1}</span>
-            </div>
-          </div>
-        </div>
-        {/each}
-        <!-- product end  -->
-      </div>
-    </div>
-  </div>
+	<div class="bg-white py-6 sm:py-8 lg:py-12">
+		<div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+			<div class="flex justify-between items-end gap-4 mb-6">
+				<h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">
+					Ingredients
+				</h2>
+			</div>
+			<label for="searchInput">Search</label>
+			<input
+				type="text"
+				bind:value={val}
+				id="searchInput"
+				class="w-full flex-1 bg-gray-50 text-gray-800 placeholder-gray-500 border focus:ring ring-orange-300 rounded outline-none transition duration-100 px-3 py-2"
+			/>
+			<div class="grid gap-x-4 md:gap-x-6 gap-y-8">
+				<ul>
+					{#each $filtered as item}
+						<li>
+							{item}
+						</li>
+					{/each}
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
