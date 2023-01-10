0564823cebb3771b6e46bf999b6fb348244a8f1f
grantgariepy <grantgariepy@protonmail.com>
2023-01-09 19:59:41 -0700

    trying different search list

--- src/routes/ingredients/+page.svelte
+++ src/routes/ingredients/+page.svelte
@@ -7,7 +7,6 @@
 	array.forEach((element: any) => {
 		drinkArray.push(element.strIngredient1.toLowerCase());
 	});
-	console.log(drinkArray);
 	const term = writable('');
 	const items = writable(drinkArray);
 	const filtered = derived([term, items], ([$term, items]) =>
@@ -25,21 +24,21 @@
 					Ingredients
 				</h2>
 			</div>
-			<label for="searchInput">Search</label>
-			<input
-				type="text"
-				bind:value={val}
-				id="searchInput"
-				class="w-full flex-1 bg-gray-50 text-gray-800 placeholder-gray-500 border focus:ring ring-orange-300 rounded outline-none transition duration-100 px-3 py-2"
-			/>
-			<div class="">
-				<ul class="list-none">
+			<div class="flex justify-center">
+				<ul
+					class="bg-white rounded-lg border border-gray-200 w-96 text-gray-900"
+				>
+					<input
+						placeholder="Search"
+						type="text"
+						bind:value={val}
+						id="searchInput"
+						class="w-full flex-1 bg-gray-50 text-gray-800 placeholder-gray-500 border focus:ring ring-orange-300 rounded outline-none transition duration-100 px-3 py-2"
+					/>
 					{#each $filtered as item}
-						<div class="">
-							<li>
-								<a class="text-2xl" href="ingredients/{item}">{item}</a>
-							</li>
-						</div>
+						<li class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg">
+							<a class="text-2xl" href="ingredients/{item}">{item}</a>
+						</li>
 					{/each}
 				</ul>
 			</div>
